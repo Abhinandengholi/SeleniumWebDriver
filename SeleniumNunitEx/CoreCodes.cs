6570cffb6d674bf0532ce372c7d88810d83ac7f1
Abhinandengholi <Abhinand.engholi@ust.com>
2023-11-16 12:55:01 +0530

    16/11/2023

---
+++ SeleniumNunitEx/CoreCodes.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V117.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumNunitEx
+{
+    internal class CoreCodes
+    {
+        Dictionary<string, string>? properties;
+        IWebDriver driver;
+        public void ReadConfigSettings() 
+        {
+            string currDir = Directory.GetParent(@"../../../").FullName;//getting the current directory
+            properties = new Dictionary<string, string>();//declaring  the dictionary
+            string filename = currDir + "/config/config.properties";//taking the file from wworking directory
+            string[] lines = File.ReadAllLines(filename);
+            foreach (string line in lines)//for getting file data even if there are whitespace
+            {
+                if(!string.IsNullOrWhiteSpace(line)&& line.Contains("="))
+                {
+                    string[] parts=line.Split('=');
+                    string key = parts[0].Trim();
+                    string value = parts[1].Trim();
+                    properties[key] = value;
+                }
+            }
+        }
+        [OneTimeSetUp]
+        public void InitializeBrowser()
+        {
+            foreach(var prop in properties)
+            {
+                if (properties["browser"].ToLower()=="chrome")
+                {
+                    driver = new ChromeDriver();
+                }
+            }
+        }
+        [OneTimeTearDown]   
+        public void Cleanup()
+        {
+
+        }
+    }
+}
---
+++ SeleniumNunitEx/GoogleHPTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumNunitEx
+{
+    [TestFixture]
+    internal class GoogleHPTests
+    {
+        [Test]
+        public void Titletest()
+        {
+            Thread.Sleep(2000); 
+            Assert.AreEqual("Google", driver.Title);
+            Console.WriteLine("Test Passed");
+        }
+    }
+}
