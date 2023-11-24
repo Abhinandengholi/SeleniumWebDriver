f2e4dcd62ff9e575e6b40e2d030524d9f193c830
Abhinandengholi <Abhinand.engholi@ust.com>
2023-11-24 14:27:45 +0530

    24/11/2023

--- SeleniumNunitEx/GoogleHPTests.cs
+++ SeleniumNunitEx/GoogleHPTests.cs
@@ -8,56 +8,66 @@ using System.Threading.Tasks;
 namespace SeleniumNunitEx
 {
     [TestFixture]
-    internal class GoogleHPTests:CoreCodes
+    internal class GoogleHPTests : CoreCodes
     {
-        [Ignore("other")]
+        [Ignore(" ")]
         [Test]
         [Order(10)]
         public void Titletest()
         {
-            Thread.Sleep(2000); 
+            Thread.Sleep(2000);
             Assert.AreEqual("Google", driver.Title);
             Console.WriteLine("Title Test Passed");
         }
-        [Ignore("other")]
+
         [Test]
         [Order(20)]
         public void GStest()
         {
-            IWebElement Searchinputtextbox = driver.FindElement(By.Id("APjFqb"));
-            Searchinputtextbox.SendKeys("hp laptop");
-            Thread.Sleep(3000);
-            IWebElement searchbutton = driver.FindElement(By.ClassName("gNO89b"));       //Name("btnK"));
-            searchbutton.Click();
-            Assert.AreEqual("hp laptop - Google Search", driver.Title);
-            Console.WriteLine("GS-Test passed");
-        }
-        [Ignore("other")]
-        [Test]
-        public void CheckAllLinksStatusTest()
-        {
-            List<IWebElement> allLinks = driver.FindElements(By.TagName("a")).ToList();
-            foreach(var link in allLinks)
+            string? currDir = Directory.GetParent(@"../../../").FullName;
+            string? excelFilePath = currDir + "\\InputData.xlsx";
+            Console.Write(excelFilePath);
+            List<ExcelData> excelDataList = ExcelUtils.ReadExcelData(excelFilePath);
+            foreach (var excelData in excelDataList)
             {
-                string url=link.GetAttribute("href");
-                if (url == null)
+                Console.WriteLine($"Text:{excelData.SearchText}");
+                IWebElement Searchinputtextbox = driver.FindElement(By.Id("APjFqb"));
+                Searchinputtextbox.SendKeys(excelData.SearchText);
+                Thread.Sleep(3000);
+                IWebElement searchbutton = driver.FindElement(By.ClassName("gNO89b"));       //Name("btnK"));
+                searchbutton.Click();
+                Assert.That(driver.Title, Is.EqualTo(excelData.SearchText + " - Google Search"));
+                Console.WriteLine("GS-Test passed");
+                driver.Navigate().GoToUrl("https://www.google.com/");
+            }
+        }
+            [Ignore(" ")]
+            [Test]
+            public void CheckAllLinksStatusTest()
+            {
+                List<IWebElement> allLinks = driver.FindElements(By.TagName("a")).ToList();
+                foreach (var link in allLinks)
                 {
-                    Console.WriteLine("URL is null");
-                    continue;
-                }
-                else
-                {
-                    bool isworking = CheckLinkStatus(url);
-                    if (isworking)
+                    string url = link.GetAttribute("href");
+                    if (url == null)
                     {
-                        Console.WriteLine(url + "is working");
+                        Console.WriteLine("URL is null");
+                        continue;
                     }
                     else
                     {
-                        Console.WriteLine(url + "is not working");
+                        bool isworking = CheckLinkStatus(url);
+                        if (isworking)
+                        {
+                            Console.WriteLine(url + "is working");
+                        }
+                        else
+                        {
+                            Console.WriteLine(url + "is not working");
+                        }
                     }
                 }
             }
         }
     }
-}
+
